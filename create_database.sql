58ddb5dd215b97c266d1bd664c83efbf1664fe15
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-15 08:47:19 -0400

    Added sql file to create the database tables

---
+++ create_database.sql
@@ -0,0 +1,138 @@
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: cake
+-- ------------------------------------------------------
+-- Server version	5.0.75-0ubuntu10.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aliases`
+--
+
+DROP TABLE IF EXISTS `aliases`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `aliases` (
+  `first_name` varchar(255) character set latin1 default NULL,
+  `initial` varchar(15) character set latin1 default NULL,
+  `last_name` varchar(255) character set latin1 default NULL,
+  `id` int(11) NOT NULL auto_increment,
+  `author_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `author_id` (`author_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 3072 kB; (`author_id`) REFER `cake/authors`(`id';
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `aliases`
+--
+
+LOCK TABLES `aliases` WRITE;
+/*!40000 ALTER TABLE `aliases` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aliases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aliases_papers`
+--
+
+DROP TABLE IF EXISTS `aliases_papers`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `aliases_papers` (
+  `alias_id` int(11) NOT NULL,
+  `paper_id` int(11) NOT NULL,
+  KEY `alias_id` (`alias_id`),
+  KEY `paper_id` (`paper_id`),
+  CONSTRAINT `aliases_papers_ibfk_1` FOREIGN KEY (`alias_id`) REFERENCES `alias` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `aliases_papers_ibfk_2` FOREIGN KEY (`paper_id`) REFERENCES `papers` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `aliases_papers`
+--
+
+LOCK TABLES `aliases_papers` WRITE;
+/*!40000 ALTER TABLE `aliases_papers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aliases_papers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `authors`
+--
+
+DROP TABLE IF EXISTS `authors`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL auto_increment,
+  `first_name` varchar(255) default NULL,
+  `initial` varchar(15) default NULL,
+  `last_name` varchar(255) default NULL,
+  `email` varchar(255) default NULL,
+  `homepage` varchar(255) default NULL,
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Author table';
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `authors`
+--
+
+LOCK TABLES `authors` WRITE;
+/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `papers`
+--
+
+DROP TABLE IF EXISTS `papers`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `papers` (
+  `id` int(11) NOT NULL auto_increment,
+  `tr-id` varchar(255) NOT NULL COMMENT 'Tech report ID',
+  `title` varchar(500) NOT NULL,
+  `published_at` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `update_at` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `pdf` varchar(255) default NULL,
+  `ps` varchar(255) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `papers`
+--
+
+LOCK TABLES `papers` WRITE;
+/*!40000 ALTER TABLE `papers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `papers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2009-07-15 12:45:30
