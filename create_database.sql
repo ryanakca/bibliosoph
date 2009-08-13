f8a63156536cd67c6d5b4751c34ab8f2a51ebe13
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-08-13 10:50:00 -0400

    Updated create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: cake
 -- ------------------------------------------------------
--- Server version	5.0.75-0ubuntu10.2
+-- Server version       5.0.75-0ubuntu10.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,15 +23,23 @@ DROP TABLE IF EXISTS `aliases`;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `aliases` (
-  `first_name` varchar(255) default NULL,
-  `initial` varchar(15) default NULL,
-  `last_name` varchar(255) default NULL,
+  `name` varchar(255) NOT NULL,
   `id` int(11) NOT NULL auto_increment,
   `author_id` int(11) NOT NULL,
+  `updated` datetime NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `author_id` (`author_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `aliases`
+--
+
+LOCK TABLES `aliases` WRITE;
+/*!40000 ALTER TABLE `aliases` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aliases` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `aliases_papers`
@@ -44,8 +52,17 @@ CREATE TABLE `aliases_papers` (
   `alias_id` int(11) unsigned NOT NULL default '0',
   `paper_id` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`alias_id`,`paper_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `aliases_papers`
+--
+
+LOCK TABLES `aliases_papers` WRITE;
+/*!40000 ALTER TABLE `aliases_papers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aliases_papers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `authors`
@@ -61,10 +78,19 @@ CREATE TABLE `authors` (
   `last_name` varchar(255) default NULL,
   `email` varchar(255) default NULL,
   `homepage` varchar(255) default NULL,
-  `updated_on` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='Author table';
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='Author table';
 SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `authors`
+--
+
+LOCK TABLES `authors` WRITE;
+/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `papers`
@@ -78,14 +104,75 @@ CREATE TABLE `papers` (
   `tr-id` varchar(255) NOT NULL COMMENT 'Tech report ID',
   `title` varchar(500) NOT NULL,
   `published_on` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `update_on` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `pdf` varchar(255) default NULL,
-  `ps` varchar(255) default NULL,
-  `pages` int(5) UNSIGNED default NULL
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `pages` int(5) unsigned default NULL,
+  `pdf_id` int(11) default NULL,
+  `ps_id` int(11) default NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `tr-id` (`tr-id`)
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
 
+--
+-- Dumping data for table `papers`
+--
+
+LOCK TABLES `papers` WRITE;
+/*!40000 ALTER TABLE `papers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `papers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uploads`
+--
+
+DROP TABLE IF EXISTS `uploads`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `uploads` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(200) NOT NULL,
+  `type` varchar(200) NOT NULL,
+  `size` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `paper_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=143 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `uploads`
+--
+
+LOCK TABLES `uploads` WRITE;
+/*!40000 ALTER TABLE `uploads` DISABLE KEYS */;
+/*!40000 ALTER TABLE `uploads` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -96,4 +183,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-07-16 14:56:51
+-- Dump completed on 2009-08-13 14:24:19
