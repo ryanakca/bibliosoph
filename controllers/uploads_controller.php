9589047bdf494e5b023869dbef764d408744bc90
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-19 12:09:25 -0400

    New version of fileUpload plugin did a s/uploadedFile/currentFile

--- controllers/uploads_controller.php
+++ controllers/uploads_controller.php
@@ -23,18 +23,18 @@ class UploadsController extends AppController {
                         $paper = $this->Upload->Paper->find('first', array('id' => $this->data['Upload']['paper_id']));
                         /* We don't want to upload a new PDF for the paper if we already have 
                          * one. */
-                        if (($this->FileUpload->uploadedFile['type'] == 'application/pdf') && isset($paper['Paper']['pdf_id'])) {
+                        if (($this->FileUpload->currentFile['type'] == 'application/pdf') && isset($paper['Paper']['pdf_id'])) {
                                 $this->Session->setFlash(__('This paper already has a PDF, please delete it first.', true));
                                 $this->redirect(array('action'=>'index'));
                         /* Same with PS */
-                        } elseif ((in_array($this->FileUpload->uploadedFile['type'], 
+                        } elseif ((in_array($this->FileUpload->currentFile['type'], 
                                     array('application/postscript', 'application/ps')) && isset($paper['Paper']['ps_id']))) {
                                 $this->Session->setFlash(__('This paper already has a PS, please delete it first.', true));
                                 $this->redirect(array('action'=>'index'));
                         } else {
                                 $this->FileUpload->fileName = $paper['Paper']['tr-id']. '.' . $this->FileUpload->_ext();
                                 $this->FileUpload->processFile();
-                                if ($this->FileUpload->uploadedFile['type'] == 'application/pdf') {
+                                if ($this->FileUpload->currentFile['type'] == 'application/pdf') {
                                     $paper['Paper']['pdf_id'] = $this->FileUpload->uploadId;
                                 } else {
                                     $paper['Paper']['ps_id'] = $this->FileUpload->uploadId;
